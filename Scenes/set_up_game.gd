bc60a010b7291f44a559d1ce95a63bec97d66fd6
LeoBosse <bosse.leo@laposte.net>
2025-07-29 17:00:18 +0200

    Added rule choices in the setup scene (no effects)

--- Scenes/set_up_game.gd
+++ Scenes/set_up_game.gd
@@ -7,24 +7,25 @@ extends Control
 func _on_nb_players_text_changed(new_text: String) -> void:
 	if new_text.is_valid_int():
 		nb_players = int(new_text)
-		
 		UpdateSettings()
-		$PlayerNames.visible = true
-		$CreateGameButton.visible = true
+		
+		%PlayerNames.visible = true
+		%Rules.visible = true
+		%CreateGameButton.visible = true
 
 
 func UpdateSettings():
 	for i in range(1, nb_players):
 		print("adding player")
-		var new_player = $"PlayerNames/Joueur 1".duplicate(8)
+		var new_player = %PlayerNames/"Joueur 1".duplicate(8)
 		new_player.get_node("Label").text = "Joueur " + str(i+1)
 		new_player.get_node("TextEdit").placeholder_text = "Joueur " + str(i+1)
-		$PlayerNames.add_child(new_player)
+		%PlayerNames.add_child(new_player)
 
 func GetPlayers() -> Array:
 	var players:Array = []
 	var name:String = ""
-	for j in $PlayerNames.get_children():
+	for j in %PlayerNames.get_children():
 		name = j.get_child(1).text.to_lower()
 		if name:
 			players.append(name)
