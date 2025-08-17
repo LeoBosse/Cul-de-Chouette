d0127f965014f7ef3ab0b6e63593e6f0b92cd58c
LeoBosse <bosse.leo@laposte.net>
2025-08-17 22:49:06 +0200

    cannot modify first player

--- Scenes/rules/souflette.gd
+++ Scenes/rules/souflette.gd
@@ -34,16 +34,16 @@ func check_validity(dice_values:Array, _players:Array=[], _current_player:int=-1
 func SetUpPlayerOptions(players_list:Array) -> void :
 	"""Should be called to fill in the players in the dropdown menu"""
 	players_list_menu.clear()
-	$VBoxContainer/MenuButton2.clear()
+	%MenuButton2.clear()
 	for p in players_list:
 		players_list_menu.add_item(p)
-		$VBoxContainer/MenuButton2.add_item(p)
+		%MenuButton2.add_item(p)
 
 func SetPlayer(player_id:int) -> void:
 	"""Set the player concerned by the rule. Can be called to automatically set the current player."""
 	players_list_menu.select(player_id)
 	challenged_player = player_id
-	$VBoxContainer/MenuButton2.select(player_id)
+	%MenuButton2.select(player_id)
 	
 func _on_menu_button_item_selected(_index: int) -> void:
 	changed_rules.emit()
