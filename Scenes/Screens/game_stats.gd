429aa7cae6ef3a7a1a485e47f538bdcf25cc47ff
LeoBosse <bosse.leo@laposte.net>
2025-09-05 17:52:17 +0200

    added grelottine history

--- Scenes/Screens/game_stats.gd
+++ Scenes/Screens/game_stats.gd
@@ -8,13 +8,15 @@ var player_names:Array
 var player_scores:Array[Array]
 var current_player_list:Array = [0]
 var civet_history:Array = []
+var grelottine_history:Array = []
+
 
 
 @export var graph_colors:Array = ['RED', 'GREEN', 'BLUE', 'CYAN', 'ORANGE', 'GOLD', 'TEAL', 'PURPLE']
 @export var graph_size:Vector2 = Vector2(343, 343)
 @export var graph_origin:Vector2 = Vector2(0, graph_size.y)
 
-signal undoing_turn(point_correction:Array, current_player:int, civets:Array)
+signal undoing_turn(point_correction:Array, current_player:int, civets:Array, grelottines:Array)
 
 func Setup(player_names_list:Array):
 	nb_players = len(player_names_list)
@@ -23,9 +25,11 @@ func Setup(player_names_list:Array):
 	player_names = player_names_list.duplicate()
 	player_scores.resize(nb_players)
 	civet_history.resize(nb_players)
+	grelottine_history.resize(nb_players)
 	for i in range(nb_players):
 		player_scores[i] = [0]
 		civet_history[i] = [false]
+		grelottine_history[i] = [false]
 		AddEntryToScoreTable(player_names[i])
 	for i in range(nb_players):
 		AddEntryToScoreTable(player_scores[i][0])
@@ -37,6 +41,7 @@ func RegisterNewState(players:Array, current_player:int):
 	for i in range(nb_players):
 		player_scores[i].append(players[i].score)
 		civet_history[i].append(players[i].has_civet)
+		grelottine_history[i].append(players[i].has_grelottine)
 		
 		AddEntryToScoreTable(players[i].score)
 		AddScoreToGraph(i, players[i].score)
@@ -54,7 +59,8 @@ func GetExport() -> Dictionary:
 	return {"names":player_names,
 			"scores":player_scores,
 			"current_player_list":current_player_list,
-			"civet_history":civet_history}
+			"civet_history":civet_history,
+			"grelottine_history":grelottine_history}
 			
 func Import(data:Dictionary):
 	Setup(data["names"])
@@ -66,6 +72,7 @@ func Import(data:Dictionary):
 			new_scores[p] = Player.new()
 			new_scores[p].score = data["scores"][p][i]
 			new_scores[p].has_civet = data["civet_history"][p][i]
+			new_scores[p].has_grelottine = data["grelottine_history"][p][i]
 		RegisterNewState(new_scores, data["current_player_list"][i])
 
 func GetWinnerName() -> String:
@@ -103,16 +110,17 @@ func UndoTurn():
 		return
 	
 	var correction:Array = []
-	print(civet_history)
+	#print(civet_history)
 	for i in range(nb_players):
 		correction.append(player_scores[i][-2] - player_scores[i][-1])
 		player_scores[i].pop_back()
 		civet_history[i].pop_back()
+		grelottine_history[i].pop_back()
 		%ScoreTableGrid.get_child(%ScoreTableGrid.get_child_count() - i - 1).queue_free()
 		%GraphScore.RemovePointFromLine(i, -1, false)
 	%GraphScore.AdaptScalingToLines(true, false)
-	print(civet_history)
-	undoing_turn.emit(correction, current_player_list[-1], civet_history.map(func(x):return x[-1]))
+	#print(civet_history)
+	undoing_turn.emit(correction, current_player_list[-1], civet_history.map(func(x):return x[-1]), grelottine_history.map(func(x):return x[-1]))
 	
 	current_player_list.pop_back()
 
